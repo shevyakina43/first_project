43e650391c25386a10f3ec180c2c6c7529adc0b1
shevyakina43 <shevyakina43@gmail.com>
2025-10-27 13:46:44 +0100

    lesson_27_10.py

--- lesson_27_10.py
+++ lesson_27_10.py
@@ -67,6 +67,35 @@
 ###
 ####
 
+# n = int(input("Count: "))
+# # for el in range(1, n+1):
+# #     print("#" * el)
+
+# while n > 0:
+#     print("#" * n)
+#     n -= 1 # n = n - 1
+
+
+# 9
+
+# а даним цілим додатнім числом n обчисліть n! -
+# значення факторіалу цього числа.
+# Факторіал: n! = 1 * 2 * 3 * ... * (n-1) * n
 n = int(input("Count: "))
-# for el in range(1, n+1):
-#     print("#" * el)
+
+f= 1
+for i in range(1, n+1):
+    print(i)
+    f *= i
+
+print(f)
+
+print("---------")
+
+f = 1 
+while n > 0:
+    print(n)
+    f *= n
+    n -= 1
+
+print(f)
