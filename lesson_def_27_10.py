96b7f4a8fdbecbb04d5298c2ff6a88bcd6948f27
shevyakina43 <shevyakina43@gmail.com>
2025-10-28 16:02:46 +0100

    lesson_def_27_10.py

--- lesson_def_27_10.py
+++ lesson_def_27_10.py
@@ -34,3 +34,54 @@
 #         return word[:n]
 # print(n_letter("letter", 3))
 
+# 5
+# def find_max(a, b, c):
+#     return max(a, b, c)
+
+# a = int(input("a: "))
+# b = int(input("b: "))
+# c = int(input("c: "))
+
+# largest_number = find_max(a, b, c)
+# print(f"Найбільше число: {largest_number}")
+
+
+# 6
+# def teg_html(teg, text):
+#     return f"<{teg}>{text}</{teg}>"
+
+# text = input("Enter text: ")
+# # 1 metod
+# teg, string = text.split()
+# # 2 metod
+# teg = text.split()[0]
+# string = " ".join(text.split()[1:])
+
+# print(teg_html(teg, string))
+
+
+# 10
+def rainfall_statistics(values):
+    month = ["January", "February", 
+        "March", "April", "May", "June", 
+        "July", "August", "September", 
+        "October", "November", "December"
+        ]
+    rain = list(map(float, values.split()))
+
+    total = sum(rain)
+
+    average = total / 12 # len(rain) = 12
+
+    max_rain = max(rain)
+    min_rain = min(rain)
+
+    max_month = months[rain.index(max_rain)]
+    min_month = months[rain.index(min_rain)]
+
+    return (total, average, (max_rain, max_month), (min_rain, min_month))
+
+data = "22 22 24 49 72 98 101 82 51 40 36 24"
+
+result = rainfall_statistics(data)
+print(result)
