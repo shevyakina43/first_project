87a65403111d0576ddffc1c7271beeb3bb83f770
shevyakina43 <shevyakina43@gmail.com>
2025-11-03 16:02:47 +0100

    lesson_set_dict.py

---
+++ lesson_set_dict.py
@@ -0,0 +1,70 @@
+# # 1
+# def task1():
+#     to_do = {
+#         "today": ["read", "clear", "dog"],
+#         "tomorrow": ["read", "call my mom"]
+#     }
+#     # print(to_do)
+#     print("To do for today:")
+#     for item in to_do["today"]:
+#         print("-", item)
+        
+#     print("To do for tomorrow:")
+#     for item in to_do["tomorrow"]:
+#         print("-", item)
+        
+# # task1()
+
+
+# # 2
+# user_id = int(input("Your id: "))
+
+# if user_id in users:
+#     print(f"Hello, {user[user_id]}! ")
+# else:
+#     print("Hello, all")
+
+# 3
+films = {'Avengers: Endgame': 2019,
+    'Guardians of the Galaxy': 2014,
+    'Iron Man': 2008,
+    'Thor': 2011}
+
+# sorted(films.items())
+sorted_films = dict(sorted(films.items()))
+
+# print(films)
+# print(sorted(films.items()))
+# print(sorted_films)
+# for name, year in sorted_films.items():
+#     print(f"('{name}': {year})")
+
+# for name in sorted_films.items():
+#     print(f"('{name}')
+
+
+
+# 4
+# def task4():
+
+# n =int(input("n: "))
+
+# # squares = {} # dict()
+# # for i in range(1, n+1):
+# #     # dict[key] = value
+# #     squares[i] = i ** 2
+
+# squares = {i: i ** 2 for i in range(1, n+1)}
+
+# print(squares)
+
+# 5
+def task5():
+    weeks = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+days = 
+
+n = 
+for name, year in week_dict.items()
+    if number == n:
+        print(day)
+        break
