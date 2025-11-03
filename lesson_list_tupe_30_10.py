2b1fef9d05ddadc719e30e1d5bd884c9011ce5a7
shevyakina43 <shevyakina43@gmail.com>
2025-11-03 10:32:13 +0100

    lesson_list_tupe_30_10.py

---
+++ lesson_list_tupe_30_10.py
@@ -0,0 +1,30 @@
+# 6
+text = "New Dehli New York Paris Prague Reykjavik"
+
+text = text.split()
+
+new_text = []
+
+for word in text:
+    print(word)
+    new_text.append()
+
+for word in text:
+    print(word)
+
+for word_i in range(len(text)):
+    print(word_i)
+
+
+
+# 10
+while:
+    line = in
+
+
+
+# 18
+text = "horse, cat, parrot, goldfish, dog"
+text_list = text_split(", ")
+text_list.sort(revers=True)
+" ".join
