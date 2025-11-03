24b697a2fae0dbd9826f22cf505d373a7db42d49
shevyakina43 <shevyakina43@gmail.com>
2025-11-03 12:06:15 +0100

    lesson_03_11.py

---
+++ lesson_03_11.py
@@ -0,0 +1,19 @@
+# 12
+# n = 0
+# result = 1
+# while n < len(numbers):
+#     result *= numbers[n] # result = result * numbers[n]
+#     n += 1
+ 
+# print(result)
+
+# 16
+numbers = list(map(int, input("Enter list: ").split()))
+index = 4
+
+# while len(numbers) > index:
+while numbers:
+    # index %= len(numbers)  
+    index2 = index % len(numbers)
+    numbers.pop(index2)
+    print(numbers)
