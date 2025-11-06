1439861569e72af537aad62ce74501785445a95f
shevyakina43 <shevyakina43@gmail.com>
2025-11-06 10:44:49 +0100

    task_set_dict.py

---
+++ task_set_dict.py
@@ -0,0 +1,53 @@
+stocks = {"IBM": 205.55,
+    "FB": 10.75,
+    "ACME": 45.23,
+    "AAPL": 612.78,
+    "HPQ": 37.2
+    }
+
+# print(stocks.value()) # добавляется список
+# print(sorted(list(stocks.value())))
+# for v in stocks.value():
+#     print(v)
+
+# print(sorted(list(stocks.value()), key=))
+# print(sorted(stocks.value()), key=) # сортировка за ключами
+
+# item ->(key, value)
+# item1 ->value
+def get_value(item):
+    return item[1]
+
+# print(type(stocks.items()))
+# print(stocks.items())
+# print(list(stocks.items())) #пройдись по отсортированному списки, и выдай ключ
+# print(sorted(stocks.items(), key=get_value)) #отсортировано по значению от меньшего к большему 
+
+
+for key, value in stocks.items():
+    print(value, key)
+
+
+# stocks = 
+# get_value - 
+# for key, value in stocks.items() - 
+# print(value, key) - 
+
+
+
+# 14
+# ['a', 'bb', 'acD', 'bb', 'abc', 'ac', 'BCD', 'a']
+text = "a bb acD bb abc ac BCD a".lover().split() #сначала вывели буквы в маленький формат, потом розбиваем и выводим в списк, до результата исполнения
+# text_set = set(text) # {'a', 'bb', 'acD', 'abc', 'ac', 'BCD'}
+
+result = {}
+for word in text:
+    # result[word] = text.count(word)  # перезаписывает значение
+    result[word] = resulr.get(word, 0) + 1 #перезаписываются данные более корректно
+
+
+
+for key, value in result.items():
+    print(key, value)
+
+
